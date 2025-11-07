c59b62e61dd3c5ed69a2e4206887044c4ac7fc8b
Rafaela Aldrigues <fafa_bicalho@hotmail.com>
2025-11-07 20:58:32 -0300

    feat: adiciona entidades Pagamento, StatusPagamento e MetodoPagamento

---
+++ pagamentos-api/src/main/java/org/fadesp/pagamentos_api/enums/MetodoPagamento.java
@@ -0,0 +1,8 @@
+package org.fadesp.pagamentos_api.enums;
+
+public enum MetodoPagamento {
+    BOLETO,
+    PIX,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO
+}
---
+++ pagamentos-api/src/main/java/org/fadesp/pagamentos_api/enums/StatusPagamento.java
@@ -0,0 +1,7 @@
+package org.fadesp.pagamentos_api.enums;
+
+public enum StatusPagamento {
+    PENDENTE,
+    PROCESSADO_SUCESSO,
+    PROCESSADO_FALHA
+}
