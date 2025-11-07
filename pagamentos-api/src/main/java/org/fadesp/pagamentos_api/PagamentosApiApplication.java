6a839386c58703bd47194a6a669d3c6ed8a3d842
Rafaela Aldrigues <fafa_bicalho@hotmail.com>
2025-11-07 20:48:49 -0300

    init: configurações iniciais da API de pagamentos

---
+++ pagamentos-api/src/main/java/org/fadesp/pagamentos_api/PagamentosApiApplication.java
@@ -0,0 +1,13 @@
+package org.fadesp.pagamentos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosApiApplication.class, args);
+	}
+
+}
